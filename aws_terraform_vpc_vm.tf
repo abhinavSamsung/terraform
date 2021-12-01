aa29b4a59ba3563f4fe8bb4fc45197e7c0f76126
Singh <abhinav.g.singh@capgemini.com>
2021-12-01 16:14:59 +0530

    Terraform file with vpc, vm, security_group

--- aws_terraform_vpc_vm.tf
+++ aws_terraform_vpc_vm.tf
@@ -4,8 +4,42 @@ provider "aws" {
 }
 resource "aws_default_vpc" "default" {}
 
+resource "aws_security_group" "transformation" {
+  name        = "transformation"
+  description = "Allow standard http and https ports inbound and everything outbound"
+  
+  ingress {
+    from_port  = 80
+    to_port    = 80
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port  = 443
+    to_port    = 443
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Terraform" : "true"    
+  }
+}
 
 resource "aws_instance" "transformation" {
   ami           = "ami-0ff8a91507f77f867"
   instance_type = "t3.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.transformation.id
+  ]
+
+  tags = {
+    "Terraform" = "true"
+  }
 }
