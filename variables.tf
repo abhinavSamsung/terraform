cd2ac996acbcd49c8cadf4d5916ea968ce454788
abhinavSamsung <94831777+abhinavSamsung@users.noreply.github.com>
2022-01-12 21:15:09 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_region" {
+  default = "us-east-1"
+}
+
+variable "ec2_image" {
+  default = "ami-0ff8a91507f77f867"
+}
+
+variable "ec2_instance_type" {
+  default = "t3.micro"
+}
+
+variable "ec2_count" {
+  default = 0
+}
